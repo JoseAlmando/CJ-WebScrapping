39344e84a2d6173dccd9f73bc664a91beb8b4b39
Jose Almando <josea.dominique01@gmail.com>
2022-04-18 21:02:32 -0400

    add UrlModel -> Table Urls

---
+++ WebScrapping/Models/UrlModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebScrapping.Models
+{
+    [Table("Urls")]
+    public class UrlModel
+    {
+        public string Product { get; set; }
+        public string Url { get; set; }
+        public string Selector { get; set; }
+        public bool findSuccess { get; set; }
+    }
+}
